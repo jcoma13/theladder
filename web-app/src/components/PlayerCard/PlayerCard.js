cd09dda0c2cd994d011b456bd5b9bc4220f91e6f
Jansen Comadena <jan11162@jcomadena.fios-router.home>
2020-06-12 16:59:19 -0700

    -can switch states with click of button -finished formatting text for overall record -sets color based on wins and losses

--- web-app/src/components/PlayerCard/PlayerCard.js
+++ web-app/src/components/PlayerCard/PlayerCard.js
@@ -1,5 +1,17 @@
 import Card, { CardTitle, CardContent, CardImage } from "calcite-react/Card";
 import React, { Component } from "react";
+import TextField from 'calcite-react/TextField';
+import Select from 'calcite-react/Select';
+import Slider from 'calcite-react/Slider'
+import Form, {
+    FormControl,
+    FormControlLabel,
+    FormHelperText,
+    Fieldset,
+    Legend
+  } from 'calcite-react/Form'
+import Menu, { MenuTitle, MenuItem } from 'calcite-react/Menu';
+import Button, { ButtonGroup } from 'calcite-react/Button';
 
 class PlayerCard extends Component {
   constructor(props) {
@@ -16,19 +28,38 @@ class PlayerCard extends Component {
       height = "N/A";
     } else {
       const inchesFormatted = inches ? inches : 0;
-      height = feet + "ft. " + inchesFormatted + " inches";
+      height = feet + "' " + inchesFormatted + '"';
     }
 
     //TODO: set record color based on wins vs losses
-    let recordColor = "blue";
+    var recordColor = "blue";
+    if (wins === null || losses === null) {
+        recordColor = "orange";
+    } else if (wins > losses) {
+        recordColor = "green";
+    } else if (wins < losses) {
+        recordColor = "red";
+    } else {
+        recordColor = "blue";
+    }
 
     //TODO: Format record text
     let record = "TBD";
+    if (wins === null && losses === null) {
+        record = "TBD"
+    } else if (wins === null) {
+        record = "Enter # of wins";
+    } else if (losses === null) {
+        record = "Enter # of losses"
+    } else {
+        record = wins + "-" + losses;
+    }
 
     return (
-      <Card bar="blue" style={{ margin: "0 480px", flex: "1 1 20%" }}>
+      <Card bar="blue" style={{ margin: "0 710px", flex: "1 1 20%" }}>
         <CardContent>
           <CardTitle>{name}</CardTitle>
+          <Button onClick={() => this.setState({mode: "editable"})}>Edit</Button>
           <p>
             <li>height: {height}</li>
             <li>plays: {plays}</li>
@@ -44,7 +75,45 @@ class PlayerCard extends Component {
 
   getEditableCard() {
     //TODO editable card ;-)
-    return " todo... editable";
+    return (
+        <div>
+        <TextField id="name" defaultValue={this.props.name} />
+
+        {/* height (feet)  */}
+        <Slider
+            min={0}
+            max={8}
+        />
+        {/* height (inches) */}
+        <Slider
+            min={0}
+            max={12}
+        /> 
+
+        <Select
+            onChange={this.handleSelectChange}
+            selectedValue={this.state.selectedValue}
+        >
+            <MenuItem>right-handed</MenuItem>
+            <MenuItem>left-handed</MenuItem>
+        </Select>
+        
+        <Select
+            onChange={this.handleSelectChange}
+            selectedValue={this.state.selectedValue}
+        >
+            <MenuItem>one-handed</MenuItem>
+            <MenuItem>two-handed</MenuItem>
+        </Select>
+
+        {/* wins */}
+        <TextField id="name" defaultValue={this.props.wins} />
+        {/* losses */}
+        <TextField id="name" defaultValue={this.props.losses} />
+        {/* button to switch state */}
+        <Button onClick={() => this.setState({mode: "readOnly"})}>Save</Button>
+        </div>
+    );
   }
 
   render() {
