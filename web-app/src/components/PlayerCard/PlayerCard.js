09956051452f98ab62170579382058a2a1e78650
Gary Knoll <gknoll@esri.com>
2020-06-12 12:19:19 -0600

    Ground work for Editable Card

--- web-app/src/components/PlayerCard/PlayerCard.js
+++ web-app/src/components/PlayerCard/PlayerCard.js
@@ -1,36 +1,60 @@
 import Card, { CardTitle, CardContent, CardImage } from "calcite-react/Card";
-import React from "react";
+import React, { Component } from "react";
 
-const PlayerCard = ({ name, plays, backhand, wins, losses, feet, inches }) => {
-  var height;
-  if (feet === null || height === "") {
-    height = "N/A";
-  } else {
-    const inchesFormatted = inches ? inches : 0;
-    height = feet + "ft. " + inchesFormatted + " inches";
+class PlayerCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "readOnly",
+    };
   }
 
-  //TODO: set record color based on wins vs losses
-  let recordColor = "blue";
+  getReadOnlyCard() {
+    const { name, plays, backhand, wins, losses, feet, inches } = this.props;
+    var height;
+    if (feet === null || height === "") {
+      height = "N/A";
+    } else {
+      const inchesFormatted = inches ? inches : 0;
+      height = feet + "ft. " + inchesFormatted + " inches";
+    }
 
-  //TODO: Format record text
-  let record = "TBD";
+    //TODO: set record color based on wins vs losses
+    let recordColor = "blue";
 
-  return (
-    <Card bar="blue" style={{ margin: "0 480px", flex: "1 1 20%" }}>
-      <CardContent>
-        <CardTitle>{name}</CardTitle>
-        <p>
-          <li>height: {height}</li>
-          <li>plays: {plays}</li>
-          <li>backhand: {backhand}</li>
-          <li>
-            record: <span style={{ color: recordColor }}>{record}</span>
-          </li>
-        </p>
-      </CardContent>
-    </Card>
-  );
-};
+    //TODO: Format record text
+    let record = "TBD";
+
+    return (
+      <Card bar="blue" style={{ margin: "0 480px", flex: "1 1 20%" }}>
+        <CardContent>
+          <CardTitle>{name}</CardTitle>
+          <p>
+            <li>height: {height}</li>
+            <li>plays: {plays}</li>
+            <li>backhand: {backhand}</li>
+            <li>
+              record: <span style={{ color: recordColor }}>{record}</span>
+            </li>
+          </p>
+        </CardContent>
+      </Card>
+    );
+  }
+
+  getEditableCard() {
+    //TODO editable card ;-)
+    return " todo... editable";
+  }
+
+  render() {
+    if (this.state.mode === "readOnly") {
+      return this.getReadOnlyCard();
+    } else if (this.state.mode === "editable") {
+      return this.getEditableCard();
+    }
+    return "unhandled mode" + this.state.mode;
+  }
+}
 
 export default PlayerCard;
