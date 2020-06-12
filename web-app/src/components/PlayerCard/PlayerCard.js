7555eea38bda66414c1d3b8ecacf2664f7473fa7
Gary Knoll <gknoll@esri.com>
2020-06-12 11:53:31 -0600

    modified height and record properties on Player Card. height is now feet & inches record is now wins & losses
    Added color style for record See issue #2

--- web-app/src/components/PlayerCard/PlayerCard.js
+++ web-app/src/components/PlayerCard/PlayerCard.js
@@ -1,30 +1,36 @@
-import Card, {
-    CardTitle,
-    CardContent,
-    CardImage
-  } from 'calcite-react/Card';
-import React from 'react';
+import Card, { CardTitle, CardContent, CardImage } from "calcite-react/Card";
+import React from "react";
 
-const PlayerCard = (props) => {
-    var height;
-    if (height === null || height === "") {
-        height = 'N/A';
-    } else {
-        height = props.height;
-    }
- return (
-    <Card bar="blue" style={{margin: '0 480px', flex: '1 1 20%' }}>
-        <CardContent>
-            <CardTitle>{props.name}</CardTitle>
-            <p>
-                <li>height: {height}</li>
-                <li>plays: {props.plays}</li>
-                <li>backhand: {props.backhand}</li>
-                <li>record: {props.record}</li>
-            </p>
-        </CardContent>
+const PlayerCard = ({ name, plays, backhand, wins, losses, feet, inches }) => {
+  var height;
+  if (feet === null || height === "") {
+    height = "N/A";
+  } else {
+    const inchesFormatted = inches ? inches : 0;
+    height = feet + "ft. " + inchesFormatted + " inches";
+  }
+
+  //TODO: set record color based on wins vs losses
+  let recordColor = "blue";
+
+  //TODO: Format record text
+  let record = "TBD";
+
+  return (
+    <Card bar="blue" style={{ margin: "0 480px", flex: "1 1 20%" }}>
+      <CardContent>
+        <CardTitle>{name}</CardTitle>
+        <p>
+          <li>height: {height}</li>
+          <li>plays: {plays}</li>
+          <li>backhand: {backhand}</li>
+          <li>
+            record: <span style={{ color: recordColor }}>{record}</span>
+          </li>
+        </p>
+      </CardContent>
     </Card>
- );
-}
+  );
+};
 
 export default PlayerCard;
--- web-app/src/components/PlayerCard/PlayerCard.stories.js
+++ web-app/src/components/PlayerCard/PlayerCard.stories.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import PlayerCard from './PlayerCard';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import PlayerCard from "./PlayerCard";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
 export default {
-  title: 'PlayerCard',
+  title: "PlayerCard",
   decorators: [withKnobs],
 };
 
-export const myPlayerCard = () => { 
+export const myPlayerCard = () => {
+  const name = text("Name", "Jansen Comadena");
+  const feet = number("Height(feet)", "6");
+  const inches = number("Height(inches)", "1");
+  const plays = text("Plays", "right-handed");
+  const backhand = text("Backhand", "two-handed");
+  const wins = number("Wins", 15);
+  const losses = number("Losses", 6);
 
-    const name = text('Name', 'Jansen Comadena');
-    const feet = number('Height(feet)', '6');
-    const inches = number('Height(inches)', '1');
-    const plays = text('Plays', 'right-handed');
-    const backhand = text('Backhand', 'two-handed')
-    const wins = number('Wins', 15);
-    const losses = number('Losses', 6);
-
-    return (
-
-        <PlayerCard 
-        style={{width:100}}
-        name={name}
-        height={feet + "' " + inches + '"'}
-        plays={plays}
-        backhand={backhand}
-        record={wins + '-' + losses}
-        onClick={action('clicked')}/>
-    );
-}
+  return (
+    <PlayerCard
+      style={{ width: 100 }}
+      name={name}
+      feet={feet}
+      inches={inches}
+      plays={plays}
+      backhand={backhand}
+      wins={wins}
+      losses={losses}
+      onClick={action("clicked")}
+    />
+  );
+};
 
 export const asDynamicVariables = () => {
-    const name = text('Name', 'Jansen Comadena');
-    const feet = number('Height(feet)', 6);
-    const inches = number('Height(inches)', 1);
-    const plays = text('Plays', 'right-handed');
-    const backhand = text('Backhand', 'two-handed')
-    const wins = number('Wins', 15);
-    const losses = number('Losses', 6);
-    const content = `I am ${name} and I'm ${feet + ' ft. ' + inches + ' in.'} tall. 
+  const name = text("Name", "Jansen Comadena");
+  const feet = number("Height(feet)", 6);
+  const inches = number("Height(inches)", 1);
+  const plays = text("Plays", "right-handed");
+  const backhand = text("Backhand", "two-handed");
+  const wins = number("Wins", 15);
+  const losses = number("Losses", 6);
+  const content = `I am ${name} and I'm ${
+    feet + " ft. " + inches + " in."
+  } tall. 
     I am a ${plays} tennis player with a ${backhand} backhand. My record is 
-    ${wins + '-' + losses}.`;
-    return <div>{content}</div>;
+    ${wins + "-" + losses}.`;
+  return <div>{content}</div>;
 };
