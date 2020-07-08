380bc069933c20f35670c545f0d71fd7fa66906e
Gary Knoll <gknoll@esri.com>
2020-07-08 10:53:40 -0600

    Start of getting data from server

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,14 +5,64 @@ import PlayerList from "./components/PlayerList";
 import playerData from "./data/players.json";
 
 import AddButton from "./components/AddButton";
+import { getPlayerData } from "./utils/helpers";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: playerData.players,
+      players: [
+        /* no data.. empty array */
+      ],
       open: false,
     };
+    this.init();
+  }
+
+  async init() {
+    const players = await getPlayerData();
+    const formattedPlayers = players.map((player) => {
+      /**
+       * 
+       * 
+       * 
+INPUT:
+{
+  "attributes": {
+    "Rank": 1,
+    "Name": "Jansen",
+    "Handedness": "Right",
+    "Backhand": "2-Handed",
+    "Wins": 1,
+    "Losses": 0,
+    "Feet": 6,
+    "Inches": 1,
+    "Latitude": 34.0556,
+    "Longitude": -117.1825,
+    "ObjectId": 2
+  }
+}
+
+Desired Output:
+{
+  "id": 1,
+  "name": "Jansen",
+  "feet": 6,
+  "inches": 1,
+  "plays": "right-handed",
+  "backhand": "two-handed",
+  "wins": 15,
+  "losses": 6
+},
+       * 
+       * 
+       */
+      //TODO - reformat players data and set state.
+      console.log(player);
+    });
+
+    //when done set state
+    // this.setState({ players: formattedPlayers });
   }
 
   handlePlayerAdd = (player) => {
--- web-app/src/utils/helpers.js
+++ web-app/src/utils/helpers.js
@@ -8,6 +8,25 @@ export function getNewPlayer(newId) {
     backhand: "",
     wins: null,
     losses: null,
-    mode: "editable"
+    mode: "editable",
   };
+}
+
+export async function getPlayerData() {
+  try {
+    let queryURL = new URL(
+      "https://services1.arcgis.com/dOFzdrPdRgtU4fRo/ArcGIS/rest/services/Players/FeatureServer/0/query"
+    );
+    queryURL.search = new URLSearchParams({
+      where: "1=1",
+      f: "json",
+      outFields: "*",
+      returnGeometry: false,
+    });
+    const request = await fetch(queryURL);
+    const players = await request.json();
+    return players.features;
+  } catch (e) {
+    console.error(e);
+  }
 }
