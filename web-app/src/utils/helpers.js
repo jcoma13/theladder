e303191dbe7905b7ae3ee4e461843d360691eac7
Gary Knoll <gknoll@esri.com>
2020-07-08 10:56:30 -0600

    start of update data on server

--- web-app/src/utils/helpers.js
+++ web-app/src/utils/helpers.js
@@ -30,3 +30,27 @@ export async function getPlayerData() {
     console.error(e);
   }
 }
+
+export async function updatePlayer(player) {
+  try {
+    //TODO : review https://developers.arcgis.com/rest/services-reference/apply-edits-feature-service-.htm
+    /**
+     * look at updates array
+     * Maybe needs to be a POST (search 'JavaScript Post Fetch API')
+     */
+    let queryURL = new URL(
+      "https://services1.arcgis.com/dOFzdrPdRgtU4fRo/ArcGIS/rest/services/Players/FeatureServer/0/applyEdits"
+    );
+    // queryURL.search = new URLSearchParams({
+    //   where: "1=1",
+    //   f: "json",
+    //   outFields: "*",
+    //   returnGeometry: false,
+    // });
+    // const request = await fetch(queryURL);
+    // const players = await request.json();
+    // return players.features;
+  } catch (e) {
+    console.error(e);
+  }
+}
